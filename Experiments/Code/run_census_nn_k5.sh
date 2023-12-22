c0c7817fe9a689b0c88ecc041532c0aac5424d59
Jeremy Goldwasser <jeremygoldwasser@Maras-MBP-2.localdomain>
2023-12-22 15:46:15 -0800

    lime

--- Experiments/Code/run_census_nn_k5.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=k5
-#SBATCH -o k5.out #File to which standard out will be written
-#SBATCH -e k5.err #File to which standard err will be written
-python -u /accounts/grad/jeremy_goldwasser/RankSHAP/Experiments/Code/rankshap_nn.py census 5
