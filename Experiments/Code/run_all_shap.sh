6b272fceaa4e836841d5217eb01831db748b4826
Jeremy Goldwasser <jeremygoldwasser@wifi-10-41-47-98.wifi.berkeley.edu>
2024-12-20 19:08:07 -0800

    big shell script

---
+++ Experiments/Code/run_all_shap.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+methods=("rankshap" "sprtshap")
+datasets=("census" "bank" "brca" "breast_cancer" "credit")
+ks=(2 5)
+guarantees=("rank" "set")
+alphas=(0.1 0.2)
+
+for method in "${methods[@]}"; do
+    for dataset in "${datasets[@]}"; do
+        for k in "${ks[@]}"; do
+            for guarantee in "${guarantees[@]}"; do
+                for alpha in "${alphas[@]}"; do
+                    sbatch run_top_k.sh "$method" "$dataset" "$k" "$alpha" "$guarantee"
+                done
+            done
+        done
+    done
+done
