6bef55d083dbcbbeea9007e3bb3fef66a586d9b5
Jeremy Goldwasser <jeremygoldwasser@wifi-10-41-47-98.wifi.berkeley.edu>
2024-12-20 19:11:39 -0800

    retry

--- Experiments/Code/run_top_k.sh
+++ Experiments/Code/run_top_k.sh
@@ -3,11 +3,10 @@
 if [ "$#" -ge 4 ]; then
     if [ "$#" -eq 5 ]; then
         slurm_file_name="${1}_${2}_${3}_${4}_${5}.out"
-        python -u "top_k_experiment.py" --method=$1 --dataset=$2 --k=$3 --alpha=$4 --guarantee=$5 > "$slurm_file_name"
     else
         slurm_file_name="${1}_${2}_${3}_${4}.out"
-        python -u "top_k_experiment.py" --method=$1 --dataset=$2 --k=$3 --alpha=$4 > "$slurm_file_name"
     fi
+    sbatch --output="$slurm_file_name" --wrap="python -u top_k_experiment.py --method=$1 --dataset=$2 --k=$3 --alpha=$4 ${5:+--guarantee=$5}"
 else
     echo "Usage: $0 <method> <dataset> <k> <alpha> [guarantee]"
     exit 1
