c44ad59932218c34056e4747f63d43be252028ca
Jeremy Goldwasser <jeremygoldwasser@wifi-10-41-47-98.wifi.berkeley.edu>
2024-12-20 19:13:07 -0800

    retrying

--- Experiments/Code/run_top_k.sh
+++ Experiments/Code/run_top_k.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Ensure the outputs directory exists
+mkdir -p outputs
+
 if [ "$#" -ge 4 ]; then
     if [ "$#" -eq 5 ]; then
-        slurm_file_name="${1}_${2}_${3}_${4}_${5}.out"
+        slurm_file_name="outputs/${1}_${2}_${3}_${4}_${5}.out"
     else
-        slurm_file_name="${1}_${2}_${3}_${4}.out"
+        slurm_file_name="outputs/${1}_${2}_${3}_${4}.out"
     fi
     sbatch --output="$slurm_file_name" --wrap="python -u top_k_experiment.py --method=$1 --dataset=$2 --k=$3 --alpha=$4 ${5:+--guarantee=$5}"
 else
