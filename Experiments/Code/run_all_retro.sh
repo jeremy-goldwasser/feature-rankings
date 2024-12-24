93b86ff325d3575ae7c9ae0847b461d4235e2c74
Jeremy Goldwasser <jeremygoldwasser@Jeremys-MacBook-Air.local>
2024-12-24 11:52:46 -0800

    retry

--- Experiments/Code/run_all_retro.sh
+++ Experiments/Code/run_all_retro.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-# Ensure the output directory exists
-mkdir -p retro_outputs
-
-# Parameters
-methods=("ss kernelshap") 
+methods=("ss kernelshap") # Add more methods if needed
 datasets=("census" "bank" "brca" "breast_cancer" "credit")
 
-# Loop through all combinations
 for method in "${methods[@]}"; do
     for dataset in "${datasets[@]}"; do
-        sbatch --output="retro_outputs/%x_%j.out" \
-               --error="retro_outputs/%x_%j.err" \
-               retrospective_experiment.sh "$method" "$dataset"
+        bash run_retrospective.sh "$method" "$dataset"
     done
 done
--- Experiments/Code/run_retrospective.sh
+++ Experiments/Code/run_retrospective.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-python -u "retrospective_experiment.py" --method=$1 --dataset=$2
+# Ensure the outputs directory exists
+mkdir -p retro_outputs
+
+if [ "$#" -eq 2 ]; then
+    slurm_file_name="retro_outputs/retrospective_${1}_${2}.out"
+    sbatch --output="$slurm_file_name" --wrap="python -u retrospective_experiment.py --method=$1 --dataset=$2"
+else
+    echo "Usage: $0 <method> <dataset>"
+    exit 1
+fi
