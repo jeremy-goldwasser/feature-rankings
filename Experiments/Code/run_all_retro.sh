947552f6d871a31e1d76f3b52c9bc0318fe25bc8
Jeremy Goldwasser <jeremygoldwasser@Jeremys-MacBook-Air.local>
2024-12-24 11:55:04 -0800

    minor fixes

--- Experiments/Code/run_all_retro.sh
+++ Experiments/Code/run_all_retro.sh
@@ -5,6 +5,6 @@ datasets=("census" "bank" "brca" "breast_cancer" "credit")
 
 for method in "${methods[@]}"; do
     for dataset in "${datasets[@]}"; do
-        bash run_retrospective.sh "$method" "$dataset"
+        sbatch run_retrospective.sh "$method" "$dataset"
     done
 done
