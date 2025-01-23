f146594df0d2bc37c49ed0c88db3494961703eec
Jeremy Goldwasser <jeremy_goldwasser@scf.berkeley.edu>
2025-01-22 20:47:18 -0800

    ongoing

--- Experiments/Code/run_top_k.sh
+++ Experiments/Code/run_top_k.sh
@@ -9,7 +9,7 @@ if [ "$#" -ge 4 ]; then
     else
         slurm_file_name="top_K_outputs/${1}_${2}_${3}_${4}.out"
     fi
-    sbatch --output="$slurm_file_name" --wrap="python -u top_k_experiment.py --method=$1 --dataset=$2 --k=$3 --alpha=$4 ${5:+--guarantee=$5}"
+    sbatch -p high --output="$slurm_file_name" --wrap="python -u top_k_experiment.py --method=$1 --dataset=$2 --k=$3 --alpha=$4 ${5:+--guarantee=$5}"
 else
     echo "Usage: $0 <method> <dataset> <k> <alpha> [guarantee]"
     exit 1
